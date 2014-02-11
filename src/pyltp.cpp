d3de51e6a172609e638d1ccde6a56793dd3df8c2
Oneplus <oneplus.lau@gmail.com>
2014-02-11 19:00:45 +0800

    add example, adapt to ltp3.0

--- src/pyltp.cpp
+++ src/pyltp.cpp
@@ -7,78 +7,78 @@ using namespace boost::python;
 
 struct PyLTP
 {
-private:
-	XML4NLP xml4nlp;
-	LTP ltp;
+  private:
+    XML4NLP xml4nlp;
+    LTP ltp;
 
-public:
-	PyLTP(): ltp(xml4nlp){}
-	PyLTP(string config_file): ltp(config_file.c_str(), xml4nlp){}
+  public:
+    PyLTP(): ltp() {}
+    PyLTP(string config_file): ltp(config_file.c_str()) {}
 
-	string ws(string mMsg){
-		xml4nlp.CreateDOMFromString(mMsg);
+    string ws(string mMsg){
+      xml4nlp.CreateDOMFromString(mMsg);
 
-		ltp.wordseg();
-		string result;
-		xml4nlp.SaveDOM(result);
-		xml4nlp.ClearDOM();
+      ltp.wordseg(xml4nlp);
+      string result;
+      xml4nlp.SaveDOM(result);
+      xml4nlp.ClearDOM();
 
-		return result;
-	}
+      return result;
+    }
 
-	string pos(string mMsg){
-		xml4nlp.CreateDOMFromString(mMsg);
+    string pos(string mMsg){
+      xml4nlp.CreateDOMFromString(mMsg);
 
-		ltp.postag();
-		string result;
-		xml4nlp.SaveDOM(result);
-		xml4nlp.ClearDOM();
+      ltp.postag(xml4nlp);
+      string result;
+      xml4nlp.SaveDOM(result);
+      xml4nlp.ClearDOM();
 
-		return result;
-	}
+      return result;
+    }
 
-	string ner(string mMsg){
-		xml4nlp.CreateDOMFromString(mMsg);
+    string ner(string mMsg){
+      xml4nlp.CreateDOMFromString(mMsg);
 
-		ltp.ner();
-		string result;
-		xml4nlp.SaveDOM(result);
-		xml4nlp.ClearDOM();
+      ltp.ner(xml4nlp);
+      string result;
+      xml4nlp.SaveDOM(result);
+      xml4nlp.ClearDOM();
 
-		return result;
-	}
+      return result;
+    }
 
-	string dp(string mMsg){
-		xml4nlp.CreateDOMFromString(mMsg);
+    string dp(string mMsg){
+      xml4nlp.CreateDOMFromString(mMsg);
 
-		ltp.parser();
-		string result;
-		xml4nlp.SaveDOM(result);
-		xml4nlp.ClearDOM();
+      ltp.parser(xml4nlp);
+      string result;
+      xml4nlp.SaveDOM(result);
+      xml4nlp.ClearDOM();
 
-		return result;
-	}
+      return result;
+    }
 
-	string srl(string mMsg){
-		xml4nlp.CreateDOMFromString(mMsg);
+    string srl(string mMsg){
+      xml4nlp.CreateDOMFromString(mMsg);
 
-		ltp.srl();
-		string result;
-		xml4nlp.SaveDOM(result);
-		xml4nlp.ClearDOM();
+      ltp.srl(xml4nlp);
+      string result;
+      xml4nlp.SaveDOM(result);
+      xml4nlp.ClearDOM();
 
-		return result;
-	}
+      return result;
+    }
 };
 
 BOOST_PYTHON_MODULE(pyltp)
 {
-	class_<PyLTP>("LTP", init<string>())
-			.def(init<>())
-			.def("ws", &PyLTP::ws)
-			.def("pos", &PyLTP::pos)
-			.def("ner", &PyLTP::ner)
-			.def("dp", &PyLTP::dp)
-			.def("srl", &PyLTP::srl)
-	;
+  class_<PyLTP>("LTP", init<string>())
+    .def(init<>())
+    .def("ws", &PyLTP::ws)
+    .def("pos", &PyLTP::pos)
+    .def("ner", &PyLTP::ner)
+    .def("dp", &PyLTP::dp)
+    .def("srl", &PyLTP::srl)
+    ;
 }
