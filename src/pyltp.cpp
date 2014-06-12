25964e8ebe6467c0ced5c62c8dcd5eb1315d176a
Oneplus <oneplus.lau@gmail.com>
2014-06-12 18:58:59 +0800

    ..

--- src/pyltp.cpp
+++ src/pyltp.cpp
@@ -1,84 +1,66 @@
-#include <stdlib.h>
+/**
+ * pyltp - A python extension for Language Technology Platform
+ *
+ *  @author: HuangFJ
+ *  @author: LIU, Yijia <yjliu@ir.hit.edu.cn>
+ */
+#include <iostream>
+#include <vector>
 #include <boost/python.hpp>
-#include "Xml4nlp.h"
-#include "Ltp.h"
+#include <boost/python/suite/indexing/vector_indexing_suite.hpp>
+#include "ltp/segment_dll.h"
+// #include "ltp/postagger_dll.h"
+// #include "ltp/parser_dll.h"
+// #include "ltp/ner_dll.h"
+// #include "ltp/SRL_dll.h"
 
-using namespace boost::python;
 
-struct PyLTP
-{
-  private:
-    XML4NLP xml4nlp;
-    LTP ltp;
+struct Segmentor {
+  Segmentor()
+    : model(NULL) {}
 
-  public:
-    PyLTP(): ltp() {}
-    PyLTP(string config_file): ltp(config_file.c_str()) {}
-
-    string ws(string mMsg){
-      xml4nlp.CreateDOMFromString(mMsg);
-
-      ltp.wordseg(xml4nlp);
-      string result;
-      xml4nlp.SaveDOM(result);
-      xml4nlp.ClearDOM();
-
-      return result;
+  void load(std::string model_path) {
+    if (model == NULL) {
+      model = segmentor_create_segmentor(model_path.c_str());
+    } else {
+      std::cerr << "Model reloaded!" << std::endl;
     }
+  }
 
-    string pos(string mMsg){
-      xml4nlp.CreateDOMFromString(mMsg);
-
-      ltp.postag(xml4nlp);
-      string result;
-      xml4nlp.SaveDOM(result);
-      xml4nlp.ClearDOM();
-
-      return result;
+  void load_with_lexicon(std::string model_path, std::string lexicon_path) {
+    if (model == NULL) {
+      model = segmentor_create_segmentor(model_path.c_str(), lexicon_path.c_str());
+    } else {
+      std::cerr << "Model reloaded!" << std::endl;
     }
+  }
 
-    string ner(string mMsg){
-      xml4nlp.CreateDOMFromString(mMsg);
+  std::vector<std::string> segment(std::string sentence) {
+    std::vector<std::string> ret;
+    segmentor_segment(model, sentence.c_str(), ret);
+    return ret;
+  }
 
-      ltp.ner(xml4nlp);
-      string result;
-      xml4nlp.SaveDOM(result);
-      xml4nlp.ClearDOM();
+  void release() {
+    segmentor_release_segmentor(model);
+  }
 
-      return result;
-    }
-
-    string dp(string mMsg){
-      xml4nlp.CreateDOMFromString(mMsg);
-
-      ltp.parser(xml4nlp);
-      string result;
-      xml4nlp.SaveDOM(result);
-      xml4nlp.ClearDOM();
-
-      return result;
-    }
-
-    string srl(string mMsg){
-      xml4nlp.CreateDOMFromString(mMsg);
-
-      ltp.srl(xml4nlp);
-      string result;
-      xml4nlp.SaveDOM(result);
-      xml4nlp.ClearDOM();
-
-      return result;
-    }
+  void * model;
 };
+
 
 BOOST_PYTHON_MODULE(pyltp)
 {
-  class_<PyLTP>("LTP", init<string>())
-    .def(init<>())
-    .def("ws", &PyLTP::ws)
-    .def("pos", &PyLTP::pos)
-    .def("ner", &PyLTP::ner)
-    .def("dp", &PyLTP::dp)
-    .def("srl", &PyLTP::srl)
+  using namespace boost::python;
+
+  class_< std::vector<std::string> >("VectorOfString")
+    .def(vector_indexing_suite< std::vector<std::string> >() )
+    ;
+
+  class_<Segmentor>("Segmentor")
+    .def("load", &Segmentor::load)
+    .def("load_with_lexicon", &Segmentor::load_with_lexicon)
+    .def("segment", &Segmentor::segment)
+    .def("release", &Segmentor::release)
     ;
 }
