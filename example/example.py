2e2a6b4c34695bcf44f3ad3a560fa6bb06c1bc85
liu946 <liuyang570926881@gmail.com>
2017-11-27 16:57:35 +0800

    3.4.0 with pyltp old api on mac passed

--- example/example.py
+++ example/example.py
@@ -17,33 +17,33 @@ sentence = SentenceSplitter.split(paragraph)[0]
 segmentor = Segmentor()
 segmentor.load(os.path.join(MODELDIR, "cws.model"))
 words = segmentor.segment(sentence)
-print "\t".join(words)
+print("\t".join(words))
 
 postagger = Postagger()
 postagger.load(os.path.join(MODELDIR, "pos.model"))
 postags = postagger.postag(words)
 # list-of-string parameter is support in 0.1.5
 # postags = postagger.postag(["中国","进出口","银行","与","中国银行","加强","合作"])
-print "\t".join(postags)
+print("\t".join(postags))
 
 parser = Parser()
 parser.load(os.path.join(MODELDIR, "parser.model"))
 arcs = parser.parse(words, postags)
 
-print "\t".join("%d:%s" % (arc.head, arc.relation) for arc in arcs)
+print("\t".join("%d:%s" % (arc.head, arc.relation) for arc in arcs))
 
 recognizer = NamedEntityRecognizer()
 recognizer.load(os.path.join(MODELDIR, "ner.model"))
 netags = recognizer.recognize(words, postags)
-print "\t".join(netags)
+print("\t".join(netags))
 
 labeller = SementicRoleLabeller()
-labeller.load(os.path.join(MODELDIR, "srl/"))
+labeller.load(os.path.join(MODELDIR, "pisrl.model"))
 roles = labeller.label(words, postags, netags, arcs)
 
 for role in roles:
-    print role.index, "".join(
-            ["%s:(%d,%d)" % (arg.name, arg.range.start, arg.range.end) for arg in role.arguments])
+    print(role.index, "".join(
+            ["%s:(%d,%d)" % (arg.name, arg.range.start, arg.range.end) for arg in role.arguments]))
 
 segmentor.release()
 postagger.release()
