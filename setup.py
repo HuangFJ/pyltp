ca8e9504ba19fbb21d1feae484aa9169a21d5a84
liu946 <liuyang570926881@gmail.com>
2017-11-27 16:02:48 +0800

    multithread compiling

--- setup.py
+++ setup.py
@@ -5,6 +5,24 @@ import sys
 import codecs
 from setuptools import setup, Extension
 
+# monkey-patch for parallel compilation
+def parallelCCompile(self, sources, output_dir=None, macros=None, include_dirs=None, debug=0, extra_preargs=None, extra_postargs=None, depends=None):
+    # those lines are copied from distutils.ccompiler.CCompiler directly
+    macros, objects, extra_postargs, pp_opts, build = self._setup_compile(output_dir, macros, include_dirs, sources, depends, extra_postargs)
+    cc_args = self._get_cc_args(pp_opts, debug, extra_preargs)
+    # parallel code
+    N=8 # number of parallel compilations
+    import multiprocessing.pool
+    def _single_compile(obj):
+        try: src, ext = build[obj]
+        except KeyError: return
+        self._compile(obj, src, ext, cc_args, extra_postargs, pp_opts)
+    # convert to list, imap is evaluated on-demand
+    list(multiprocessing.pool.ThreadPool(N).imap(_single_compile,objects))
+    return objects
+import distutils.ccompiler
+distutils.ccompiler.CCompiler.compile=parallelCCompile
+
 ltp_root="ltp"
 ltp_source=os.path.join(ltp_root, "src")
 ltp_thirdparty=os.path.join(ltp_root, "thirdparty")
